932c149f90efd4cb48318eb772123227b477f2d2
Amy King <amy.king@optimalworkshop.com>
2023-11-23 11:27:21 +1300

    Removed --skip-routes test

--- test/controllers/games_controller_test.rb
+++ test/controllers/games_controller_test.rb
@@ -2,12 +2,7 @@ require "test_helper"
 
 class GamesControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
-    get games_index_url
-    assert_response :success
-  end
-
-  test "should get -–skip-routes" do
-    get games_-–skip-routes_url
+    get root_path
     assert_response :success
   end
 end
