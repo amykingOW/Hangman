a8d4628aa9b270837c350426262bd74a6fcd10f7
Amy King <amy.king@optimalworkshop.com>
2023-11-28 16:23:52 +1300

    fixed print_solved_so_far GamesHelper method

--- app/helpers/games_helper.rb
+++ app/helpers/games_helper.rb
@@ -1,7 +1,7 @@
 module GamesHelper
   # Constructs a string with letters solved so far and _ for yet to be solved letters
   def print_solved_so_far(game)
-    #game.build_solved_so_far.map { |letter| letter.present? ? letter : "_" }
-    game.build_solved_so_far.map { |letter| letter || "_"}
+    #game.build_solved_so_far.map { |letter| letter.present? ? letter : "_" }.joing(" ")
+    game.build_solved_so_far.map { |letter| letter || "_"}.join(" ")
   end
 end
