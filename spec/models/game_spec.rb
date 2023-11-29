738d1be0f178f1cb23d9ce019a0eb89e6c13c573
Amy King <amy.king@optimalworkshop.com>
2023-11-29 16:31:12 +1300

    Refactored tests

--- spec/models/game_spec.rb
+++ spec/models/game_spec.rb
@@ -2,6 +2,11 @@ require '/Users/amyking/Documents/hangman/spec/rails_helper.rb'
 require '/Users/amyking/Documents/hangman/app/models/game.rb'
 
 describe Game do
+  RANDOM_WORD = "abc"
+  ABC_ARRAY = ["a", "b", "c"]
+  DEFGHIJKL_ARRAY = ["d", "e", "f", "g", "h", "i", "j", "k", "l"]
+  DEFGHIJ_ARRAY = ["d", "e", "f", "g", "h", "i", "j"]
+
   def create_game(random_word, letters_guessed)
     game = Game.new(name: "game1", random_word: random_word)
     game.guesses = create_guesses(letters_guessed)
@@ -14,48 +19,48 @@ describe Game do
 
   context "When create a new Game" do
     it "should have a name" do
-      game = create_game("abc", [])
+      game = create_game(RANDOM_WORD, [])
       expect(game).to have_attributes(name: "game1")
     end
   end
 
   context "When all letters have been guessed correctly" do
     it "won? should return true" do
-      won = create_game("abc", ["a", "b", "c"]).won?
+      won = create_game(RANDOM_WORD, ABC_ARRAY).won?
       expect(won).to eq true
     end
 
     it "in_progress? should return false" do
-      in_progress = create_game("abc", ["a", "b", "c"]).in_progress?
+      in_progress = create_game(RANDOM_WORD, ABC_ARRAY).in_progress?
       expect(in_progress).to eq false
     end
   end
 
   context "When 9 letters have been guessed incorrectly" do
     it "lost? should return true" do
-      lost = create_game("abc", ["d", "e", "f", "g", "h", "i", "j", "k", "l"]).lost?
+      lost = create_game(RANDOM_WORD, DEFGHIJKL_ARRAY).lost?
       expect(lost).to eq true
     end
 
     it "in_progress? should return false" do
-      in_progress = create_game("abc", ["d", "e", "f", "g", "h", "i", "j", "k", "l"]).in_progress?
+      in_progress = create_game(RANDOM_WORD, DEFGHIJKL_ARRAY).in_progress?
       expect(in_progress).to eq false
     end
   end
 
   context "When less than 9 letters have been guessed incorrectly" do
     it "lost? should return false" do
-      lost = create_game("abc", ["d", "e", "f", "g", "h", "i", "j"]).lost?
+      lost = create_game(RANDOM_WORD, DEFGHIJ_ARRAY).lost?
       expect(lost).to eq false
     end
 
     it "won? should return false" do
-      won = create_game("abc", ["d", "e", "f", "g", "h", "i", "j"]).won?
+      won = create_game(RANDOM_WORD, DEFGHIJ_ARRAY).won?
       expect(won).to eq false
     end
 
     it "in_progress? should return true" do
-      in_progress = create_game("abc", ["d", "e", "f", "g", "h", "i", "j"]).in_progress?
+      in_progress = create_game(RANDOM_WORD, DEFGHIJ_ARRAY).in_progress?
       expect(in_progress).to eq true
     end
   end
