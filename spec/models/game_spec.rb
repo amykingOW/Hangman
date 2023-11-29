f8a867b0839427693738cf0d497aa85cc14f83d6
Amy King <amy.king@optimalworkshop.com>
2023-11-29 15:51:37 +1300

    Refactored tests and helpers for Game.won?

--- spec/models/game_spec.rb
+++ spec/models/game_spec.rb
@@ -2,37 +2,33 @@ require '/Users/amyking/Documents/hangman/spec/rails_helper.rb'
 require '/Users/amyking/Documents/hangman/app/models/game.rb'
 
 describe Game do
-  def create_game(random_word)
+  def create_game(random_word, letters_guessed)
     game = Game.new(name: "game1", random_word: random_word)
+    game.guesses = create_guesses(letters_guessed)
+    game
   end
 
   def create_guesses(letters)
     letters.map { |letter| Guess.new(letter: letter) }
   end
 
-  def has_won?(random_word, letters_guessed)
-    game = create_game(random_word)
-    game.guesses = create_guesses(letters_guessed)
-    game.won?
-  end
-
   context "When create a new Game" do
     it "should have a name" do
-      game = create_game("abc")
+      game = create_game("abc", [])
       expect(game).to have_attributes(name: "game1")
     end
   end
 
   context "When all letters have been guessed" do
     it "won? should return true" do
-      won = has_won?("abc", ["a", "b", "c"])
+      won = create_game("abc", ["a", "b", "c"]).won?
       expect(won).to eq true
     end
   end
 
   context "When no letters have been guessed" do
     it "won? should return false" do
-      won = has_won?("abc", ["d", "e", "f"])
+      won = create_game("abc", ["d", "e", "f"]).won?
       expect(won).to eq false
     end
   end
