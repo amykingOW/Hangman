7ec4c2774fa8e2038f4f92f5be011ac65781dd83
Amy King <amy.king@optimalworkshop.com>
2023-12-01 14:17:28 +1300

    Remove irrelevant comments

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   
   # Shows message when there is a routing error
-  # https://rollbar.com/blog/ruby-on-rails-routingerror/
   def not_found
     render file: Rails.public_path.join('404.html'), status: :not_found, layout: false
   end
