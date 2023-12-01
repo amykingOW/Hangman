1a675f4a6f4cc4ddeb7322ab5b1be64e84c52b9f
Amy King <amy.king@optimalworkshop.com>
2023-12-01 15:04:26 +1300

    Add presence validation to Guess model

--- db/migrate/20231121225914_create_guesses.rb
+++ db/migrate/20231121225914_create_guesses.rb
@@ -1,7 +1,7 @@
 class CreateGuesses < ActiveRecord::Migration[7.1]
   def change
     create_table :guesses do |t|
-      t.string :letter
+      t.string :letter, null: false
       t.references :game, null: false, foreign_key: true
 
       t.timestamps
