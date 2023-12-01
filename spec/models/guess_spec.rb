2319e3a8967f1fcd7009d4a6ba0a9f03e89a127e
Amy King <amy.king@optimalworkshop.com>
2023-12-01 15:46:41 +1300

    Refactor specs for Guess model

--- spec/models/guess_spec.rb
+++ spec/models/guess_spec.rb
@@ -1,41 +1,46 @@
 require 'rails_helper'
 
 describe Guess do
-  let(:letters) { ["a", "b", "c"] }
   let(:invalid_guesses_array) { ["a", "a", "9", "*", "aa", "A"] }
-  let(:game) do
-    game = Game.new(name: "game1", random_word: "abc")
-    game.guesses = letters.map { |letter| Guess.new(letter: letter) }
-    game
-  end
+  let(:game) { Game.new(name: "game1", random_word: "abc") }
+  let(:letter) { "a" }
+  let(:guess) { Guess.new(letter: letter, game: game) }
 
   context "When validating valid letters guessed" do
     it "single lowercase letters should be valid" do
-      expect(game.guesses[0]).to be_valid
-    end
-
-    it "unique letters should be valid" do
-      expect(game.guesses[0]).to be_valid
+      expect(guess).to be_valid
     end
   end
 
-  context "When validating invalid letters guessed" do
-    let(:letters) { invalid_guesses_array }
+  context "When validating a number" do
+    let(:letter) { "9" }
 
     it "numbers should not be valid" do
-      expect(game.guesses[2]).to_not be_valid
+      expect(guess).to_not be_valid
     end
+  end
+
+  context "When validating a symbol" do
+    let(:letter) { "*" }
 
     it "symbols should not be valid" do
-      expect(game.guesses[3]).to_not be_valid
+      expect(guess).to_not be_valid
     end
+  end
+
+  context "When validating a non single letter" do
+    let(:letter) { "aa" }
 
     it "non single letters should not be valid" do
-      expect(game.guesses[4]).to_not be_valid
+      expect(guess).to_not be_valid
     end
+  end
+
+  context "When validating a capital letter" do
+    let(:letter) { "A" }
 
     it "capital letters should not be valid" do
-      expect(game.guesses[5]).to_not be_valid
+      expect(guess).to_not be_valid
     end
   end
 end
