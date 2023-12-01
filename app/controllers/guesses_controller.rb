d7e2f1f1f98f0574074baa2325d4dba29d14d53d
Amy King <amy.king@optimalworkshop.com>
2023-12-01 14:24:51 +1300

    Fix indentation

--- app/controllers/guesses_controller.rb
+++ app/controllers/guesses_controller.rb
@@ -2,13 +2,14 @@ class GuessesController < ApplicationController
   def create
     @game = Game.find(params[:game_id])
     @guess = @game.guesses.create(guess_params)
-    
+
     flash[:messages] = @guess.errors.full_messages_for(:letter)
     redirect_to game_path(@game)
   end
 
   private
-    def guess_params
-      params.require(:guess).permit(:letter)
-    end
+
+  def guess_params
+    params.require(:guess).permit(:letter)
+  end
 end
